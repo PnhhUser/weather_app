0cc21bd0027730201df6920a75eb9409d6ec95ae
PnhhUser <pnhh240300@gmail.com>
2024-05-22 18:33:55 +0700

    update

--- src/Components/inputSearch.js
+++ src/Components/inputSearch.js
@@ -1,13 +1,11 @@
 import { useRef, useState } from "react";
 import { BiSearch } from "react-icons/bi";
 import { useWeather } from "../contexts/weatherContext";
-import { sleep } from "../utils";
 
 export const InputSearch = () => {
   const searchRef = useRef();
   const [search, setSearch] = useState("");
   const { setResutlSearch } = useWeather();
-  const [isLoadingSearch, setIsLoadingSearch] = useState(false);
 
   const handleSearch = (e) => {
     let value = e.target.value;
@@ -16,12 +14,9 @@ export const InputSearch = () => {
 
   const handleEnter = async (e) => {
     if (e.keyCode === 13) {
-      setIsLoadingSearch(true);
       searchRef.current.value = "";
 
-      await sleep(3000);
       setResutlSearch(search);
-      setIsLoadingSearch(false);
     }
   };
 
@@ -36,22 +31,6 @@ export const InputSearch = () => {
           onKeyDown={handleEnter}
         />
         <BiSearch color="white" className="absolute top-3 right-2" />
-      </div>
-      <div className="h-80 flex justify-center items-center">
-        {isLoadingSearch && (
-          <div className="spinner">
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-          </div>
-        )}
       </div>
     </div>
   );
